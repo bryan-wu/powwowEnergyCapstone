382a74f9df777e3247cf6529ea01067164379a21
aivannik <anna03@ucsb.edu>
2019-10-17 18:28:55 -0700

    python template

---
+++ hello_template/run.py
@@ -0,0 +1,13 @@
+'''
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+ return 'Hello to the World of Flask!'
+if __name__ == '__main__':
+ app.run()
+ '''
+from templates import app
+#Load this config object for development mode
+app.config.from_object('configurations.DevelopmentConfig')
+app.run()
