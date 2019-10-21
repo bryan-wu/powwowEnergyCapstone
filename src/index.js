e95d0005e34d9de131d6a5e4057e2e8f79f6e324
Meredith Xu <meredithxu@169-231-91-111.wireless.ucsb.edu>
2019-10-20 17:29:33 -0700

    basic static homepage done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import HorizontalNav from './HorizontalNav';
 import './index.css';
 // class Square extends React.Component {
 //     render() {
@@ -58,16 +59,65 @@ import './index.css';
 //     }
 // }
 
-class HelloWorld extends React.Component {
-  render() {
-    return (
-      <h1>Hello, world!</h1>
+ // This component is only a placeholder for now
+ // It should be replaced with Google map area later
+  class MapArea extends Component {
+    render() {
+      return (
+        <div class="card">
+          <h5 class="card-header">placeholder</h5>
+          <div class="card-body">
+            <p class="card-text">This is only a placeholder for Google map</p>
+          </div>
+        </div>
       );
+    }
   }
-}
+
+  // Text box for Analysis text
+  class Analysis extends Component {
+    render() {
+      return (
+        <div class="card">
+          <h5 class="card-header">Water Efficiency</h5>
+          <div class="card-body">
+            <p class="card-text">analysis text goes here!</p>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  class MainBody extends Component {
+    render() {
+      return (
+        <div class="container mt-5">
+          <div class="row">
+            <div class="col-lg-6">
+              <MapArea />
+            </div>
+            <div class="col-lg-6">
+              <Analysis />
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  class Home extends Component {
+    render(){
+      return (
+        <>
+          <HorizontalNav />
+          <MainBody />
+        </>
+      );
+    }
+  }
 // ========================================
 
 ReactDOM.render(
-		<HelloWorld />,
+		<Home />,
 		document.getElementById('root')
-		);
+);
