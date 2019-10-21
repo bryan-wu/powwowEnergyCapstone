c726d992ade607705ca8cd05d659805b4f71adb1
aivannik <anna03@ucsb.edu>
2019-10-20 21:23:29 -0700

    google maps

--- src/index.js
+++ src/index.js
@@ -1,73 +1,102 @@
-import React from 'react';
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 import ReactDOM from 'react-dom';
+import HorizontalNav from './HorizontalNav';
 import './index.css';
-// class Square extends React.Component {
-//     render() {
-// 	return (
-//       <button className="square">
-// 	  {/* TODO */}
-//       </button>
-// 		);
-//     }
-// }
 
-// class Board extends React.Component {
-//     renderSquare(i) {
-// 	return <Square />;
-//     }
+const AnyReactComponent = ({ marker }) => <div>{marker}</div>;
 
-//     render() {
-// 	const status = 'Next player: X';
-
-// 	return (
-//       <div>
-//       <div className="status">{status}</div>
-//         <div className="board-row">
-//           {this.renderSquare(0)}
-//           {this.renderSquare(1)}
-//           {this.renderSquare(2)}
-//         </div>
-//         <div className="board-row">
-// 	    {this.renderSquare(3)}
-// 	    {this.renderSquare(4)}
-// 	    {this.renderSquare(5)}
-//         </div>
-//         <div className="board-row">
-// 	    {this.renderSquare(6)}
-// 	    {this.renderSquare(7)}
-// 	    {this.renderSquare(8)}
-//         </div>
-//       </div>
-// 		);
-//     }
-// }
-
-// class Game extends React.Component {
-//     render() {
-// 	return (
-//       <div className="game">
-//         <div className="game-board">
-//           <Board />
-//         </div>
-//         <div className="game-info">
-//       <div>{/* status */}</div>
-//       <ol>{/* TODO */}</ol>
-//         </div>
-//       </div>
-// 		);
-//     }
-// }
-
-class HelloWorld extends React.Component {
-  render() {
-    return (
-      <h1>Hello, world!</h1>
+ /*// This component is only a placeholder for now
+ // It should be replaced with Google map area later *
+  class MapArea extends Component {
+    render() {
+      return (
+        <div class="card">
+          <h5 class="card-header">placeholder</h5>
+          <div class="card-body">
+            <p class="card-text">This is only a placeholder for Google map</p>
+          </div>
+        </div>
       );
-  }
+    }
+  }*/
+  
+
+  class SimpleMap extends Component {
+	  static defaultProps = {
+		center: {
+		  lat: 34.42,
+		  lng: 119.69
+		},
+		zoom: 8
+		
+	  };
+	 
+	  render() {
+		return (
+		  // Important! Always set the container height explicitly
+		  <div style={{ height: '500px', width: '100%' }}>
+			<GoogleMapReact
+			  bootstrapURLKeys={{ key: 'AIzaSyCgi0P05okXVDaayvOKj80KJC2JU_p3A2k' }}
+			  defaultCenter={this.props.center}
+			  defaultZoom={this.props.zoom}
+			>
+			  <AnyReactComponent
+				lat={34.4208}
+				lng={119.6982}
+				marker="My Marker"
+			  />
+			</GoogleMapReact>
+		  </div>
+		);
+	  }
 }
+export default SimpleMap;
+
+  // Text box for Analysis text
+  class Analysis extends Component {
+    render() {
+      return (
+        <div class="card">
+          <h5 class="card-header">Water Efficiency</h5>
+          <div class="card-body">
+            <p class="card-text">analysis text goes here!</p>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  class MainBody extends Component {
+    render() {
+      return (
+        <div class="container mt-5">
+          <div class="row">
+            <div class="col-lg-6">
+              <SimpleMap />
+            </div>
+            <div class="col-lg-6">
+              <Analysis />
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  class Home extends Component {
+    render(){
+      return (
+        <>
+          <HorizontalNav />
+          <MainBody />
+        </>
+      );
+    }
+  }
 // ========================================
 
 ReactDOM.render(
-		<HelloWorld />,
+		<Home />,
 		document.getElementById('root')
-		);
+);
