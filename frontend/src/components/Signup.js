f147adf4eb4ff0221e820155e25f183812131a4a
aivannik <anna03@ucsb.edu>
2019-11-18 16:07:18 -0800

    signup page is fixed

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -50,35 +50,3 @@ export default function Signup(props) {
   );
 }
 
-
-{/*
-/*import React from 'react';
-
- 
-const Login = () => {
-    return (
-
-       <div>
-          <h1>LOG IN PAGE</h1>
-          <p>LOG IN page body content</p>
-       </div>
-    );
-}
- 
-export default Login;
-
-
-
-
-import React from 'react';
- 
-const Signup = () => {
-    return (
-       <div>
-          <h1>Sign Up</h1>
-          <p>Sign Up page body content</p>
-       </div>
-    );
-}
- 
-export default Signup;*/}
--- frontend/src/components/SimpleMap.js
+++ frontend/src/components/SimpleMap.js
@@ -1,139 +1,133 @@
-import React, { Component } from 'react';
-import coordinates1 from './CoordinatesSB'
-import coordinates2 from './CoordinatesAlameda'
-import axios from "axios";
-import { compose, withProps } from "recompose"
-import { withScriptjs, withGoogleMap, GoogleMap, Marker,Polygon } from "react-google-maps"
+import React from "react";
+import coordinates from './CoordinatesSB'
+const { compose, withProps, lifecycle } = require("recompose");
+const {
+  withScriptjs, withGoogleMap, GoogleMap, Polygon
+} = require("react-google-maps");
+const { StandaloneSearchBox } = require("react-google-maps/lib/components/places/StandaloneSearchBox");
 
-
-const GMap = compose(
-    withProps({
-        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=000",
+const SimpleMap = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=KEY&v=3.exp&libraries=geometry,drawing,places",
         loadingElement: <div style={{ height: `200%` }} />,
         containerElement: <div style={{ height: `500px` }} />,
         mapElement: <div style={{ height: `100%` }} />,
-    }),
-    withScriptjs,
-    withGoogleMap
+  }),
+  lifecycle({
+    componentWillMount() {
+      const refs = {}
+	  
 
-)((props) =>
+      this.setState({
+        places: [],
+        onSearchBoxMounted: ref => {
+          refs.searchBox = ref;
+        },
+        onPlacesChanged: () => {
+          const places = refs.searchBox.getPlaces();
+
+          this.setState({
+            places,
+          });
+        },
+      })
+    },
+  }),
+  withScriptjs  ,
+  withGoogleMap
+)(props =>
+
+<div>
 	<GoogleMap defaultZoom={7} defaultCenter={{ lat: 34.4208, lng: -119.6982 }}>
-		{console.log(props.polygons)}
-		{props.polygons}
-		{/* {Array.from(props.polygons).map(item => (
-			<Polygon
-				key={item.id}
-				path={item.coordinates}
-				options={{
-					fillColor: "#FF0000",
-					fillOpacity: 0.4,
-					strokeColor: "FF0000",
-					strokeOpacity: 1,
-					strokeWeight: 1
-				}}
-			/>
-		))} */}
-		{/*} {props.isMarkerShown && <Marker position={{ lat: 41.015137, lng: 28.979530 }} />}*/}
-		{/* <Polygon
-			path={coordinates1}
-			options={{
-				fillColor: "#FF0000",
-				fillOpacity: 0.4,
-				strokeColor: "FF0000",
-				strokeOpacity: 1,
-				strokeWeight: 1
-			}} />
 		<Polygon
-			path={coordinates2}
+			path={coordinates}
 			options={{
 				fillColor: "#FF0000",
 				fillOpacity: 0.4,
 				strokeColor: "FF0000",
 				strokeOpacity: 1,
 				strokeWeight: 1
-			}} /> */}
-	</GoogleMap>	
-		
-)
+			}} />	
+	</GoogleMap>
 
-class SimpleMap extends Component
-{
-	constructor(props) {
-		super(props);
-		this.state = {
-			fieldDataList: []
-		};
-	}
-	componentDidMount() {
-		console.log("Component did mount");
-		this.refreshList();
-		console.log(this.state.fieldDataList.length);
-		console.log("Finish component mounting");
-	}
-	refreshList = () => {
-		axios
-			.get("http://localhost:5000/field")
-			.then(res => this.setState({ fieldDataList: res.data }))
-			.catch(err => console.log(err));
-	};
-	drawPolygons = () => {
-		console.log("Draw polygons:" + this.state.fieldDataList.length);
-		var polygons = []
-		for (var i = 0; i < this.state.fieldDataList.length; ++i) {
-			if (i == 0)
-			{
-				console.log(this.state.fieldDataList[i].id);
-				console.log(this.state.fieldDataList[i].coordinates);
-			}
-			polygons.push(
-				<Polygon
-					key={this.state.fieldDataList[i].id}
-					path={this.state.fieldDataList[i].coordinates}
-					options={{
-						fillColor: "#FF0000",
-						fillOpacity: 0.4,
-						strokeColor: "FF0000",
-						strokeOpacity: 1,
-						strokeWeight: 1
-					}}
-				/>
-			);
-		}
-		console.log("Polygon return length:" + polygons.length);
-		return polygons;
-	};
-
-	render() {
-
-		return (
-			<div>
-				<GMap polygons={this.drawPolygons()} />
-				{/* <GMap polygons={this.state.fieldDataList} /> */}
-				<div className="btn-group dropright mt-2 mr-2">
-					<button className="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown">
-						Filter by Crop
-					</button>
-					<div className="dropdown-menu">
-						<button className="dropdown-item" type="button">Almond</button>
-						<button className="dropdown-item" type="button">Pistachio</button>
-						<button className="dropdown-item" type="button">Something</button>
-					</div>
-				</div>
-
-				<div className="btn-group dropright mt-2">
-					<button className="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown">
-						Filter by Year
-					</button>
-					<div className="dropdown-menu">
-						<button className="dropdown-item" type="button">year1</button>
-						<button className="dropdown-item" type="button">year2</button>
-						<button className="dropdown-item" type="button">year3</button>
-					</div>
-				</div>
+  <div data-standalone-searchbox="">
+    <StandaloneSearchBox
+      ref={props.onSearchBoxMounted}
+      bounds={props.bounds}
+      onPlacesChanged={props.onPlacesChanged}
+    >
+      <input
+        type="text"
+        placeholder="Enter Your City"
+        style={{
+          boxSizing: `border-box`,
+          border: `1px solid transparent`,
+          width: `240px`,
+          height: `32px`,
+          padding: `0 12px`,
+          borderRadius: `3px`,
+          boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+          fontSize: `14px`,
+          outline: `none`,
+          textOverflow: `ellipses`,
+        }}
+      />
+    </StandaloneSearchBox>
+	
+	<div className="btn-group dropright mt-2 mr-2">
+			<button className="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown">
+			Filter by Crop
+			</button>
+			<div className="dropdown-menu">
+				<button className="dropdown-item" type="button">Almond</button>
+				<button className="dropdown-item" type="button">Pistachio</button>
+				<button className="dropdown-item" type="button">Something</button>
 			</div>
-			
-		)
-	}
-}
+		</div>
+		
+		<div className="btn-group dropright mt-2">
+			<button className="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown">
+			Filter by Year
+			</button>
+			<div className="dropdown-menu">
+				<button className="dropdown-item" type="button">year1</button>
+				<button className="dropdown-item" type="button">year2</button>
+				<button className="dropdown-item" type="button">year3</button>
+			</div>
+		</div>
 
+
+    <ol>
+      {props.places.map(({ place_id, formatted_address, geometry: { location } }) =>
+        <li key={place_id}>
+          {formatted_address}
+          {" at "}
+          ({location.lat()}, {location.lng()})
+        </li>
+      )}
+    </ol>
+
+  </div>
+</div>
+);
+
+{/* ////
+//map autocompete tutorialhttps://medium.com/@imranhsayed/google-maps-in-react-autocomplete-location-search-draggable-marker-marker-infobox-565ab8e8cf22
+//standalone search box https://tomchentw.github.io/react-google-maps/#introduction
+import React from "react";
+import Map from './Map'
+
+class SimpleMap extends React.Component {
+  render() {
+    return(
+        <Map
+     google={this.props.google}
+     center={{lat: 34.4208, lng: -119.6982}}
+     height='500px'
+     zoom={8}
+    />
+      )
+  }
+}
+*/}
 export default SimpleMap;
